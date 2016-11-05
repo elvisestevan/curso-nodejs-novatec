70546870f7dc320beddf2f97e84bba947226648a
Elvis Estevan <elvis.estevan65@gmail.com>
2016-11-05 12:35:22 -0200

    Funcoes CRUD no DragonController

--- controller/DragonController.js
+++ controller/DragonController.js
@@ -2,8 +2,25 @@
 
 let DragonController = {
   list: function (req, res, next) {
+    response.send('Get All Dragons');
+  },
 
+  create: function (req, res, next) {
+    response.send('create a dragon');
+  },
+
+  findById: function (req, res, next) {
+    response.send('Get the dragon ' + id);
+  },
+
+  remove: function (req, res, next) {
+    response.send('Bye dragon ' + id);
+  },
+
+  update: function (req, res, next) {
+    response.send('Update the dragon ' + id);
   }
+
 };
 
 module.exports = DragonController;
